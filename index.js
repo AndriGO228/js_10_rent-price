f49a1a7a1ccbe9cf417dedec7551a8a5791317d8
andriGo <afomin129@gmail.com>
2022-11-16 07:51:37 +0200

    master

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function rentPrice(days) {
-	// write your code here
+	if (days <= 3) {
+		return days * 25;
+	} else if (days > 3 && days <= 7) {
+		return 3 * 25 + (days - 3) * 20;
+	} else  {
+		return 3 * 25 + 4 * 20 + ((days - 7) * 15);
+	}
 }
 
 module.exports = rentPrice;
